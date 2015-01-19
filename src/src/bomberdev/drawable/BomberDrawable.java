a83be2ea2563072ab7cae781951370c434a3139d
ValentinPierart <val.pierart@gmail.com>
2015-01-19 17:15:08 +0100

    Added main

--- src/src/bomberdev/drawable/BomberDrawable.java
+++ src/src/bomberdev/drawable/BomberDrawable.java
@@ -2,6 +2,7 @@ package src.bomberdev.drawable;
 
 import gameframework.drawing.Drawable;
 import gameframework.drawing.SpriteManager;
+import gameframework.drawing.SpriteManagerDefaultImpl;
 
 import java.awt.Graphics;
 import java.awt.Point;
@@ -11,24 +12,26 @@ import src.bomberdev.model.BomberEntity;
 public abstract class BomberDrawable implements Drawable {
 
 	protected final BomberEntity entity;
-	protected SpriteManager spriteManager;
-	
+	protected SpriteManagerDefaultImpl spriteManager;
+
 	public BomberDrawable(SpriteManager manager, BomberEntity entity) {
 		this.entity = entity;
 	}
-	
+
 	/**
 	 * The idle animation of the drawable.
-	 * @param direction The direction of the drawable, described by a point:<br>
-	 * <ul>
-	 * 	<li>(1,0) for right direction;</li>
-	 * 	<li>(-1,0) for left direction;</li>
-	 * 	<li>(0,1) for down direction;</li>
-	 * 	<li>(0,-1) for right direction;</li>
-	 * </ul>
+	 * 
+	 * @param direction
+	 *            The direction of the drawable, described by a point:<br>
+	 *            <ul>
+	 *            <li>(1,0) for right direction;</li>
+	 *            <li>(-1,0) for left direction;</li>
+	 *            <li>(0,1) for down direction;</li>
+	 *            <li>(0,-1) for right direction;</li>
+	 *            </ul>
 	 */
 	public abstract void animIdle(Point direction);
-	
+
 	@Override
 	public void draw(Graphics g) {
 		this.spriteManager.draw(g, this.entity.getPosition());
