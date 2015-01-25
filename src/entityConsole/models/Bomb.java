0b42b5156a4becea4b849382293f1006226d0c77
Armand (Tydax) Bour <tydaxx@gmail.com>
2015-01-25 19:03:35 +0100

    .DS_Store banished from existence.

--- src/entityConsole/models/Bomb.java
+++ src/entityConsole/models/Bomb.java
@@ -22,7 +22,7 @@ public class Bomb implements BomberEntity,Movable {
 	protected Point position;
 	protected BombDrawable drawable;
 	protected GameData data;
-	int Countdown = 75;
+	int Countdown = 25;
 	
 	public Bomb(BomberCharacter owner, Point position, GameData data) {
 		this.owner = owner;
